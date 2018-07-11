e714dc1c2292f11713bce091b3c9814685bec832
Zinia Binte Jashim <1431069@iub.edu.bd>
2018-07-11 04:40:00 +0000

    classwork06

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var http = require('http');
-var fs = require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html',function(err,data){
-      if(err){
-          return console.log("file read erroe");
-      }  
-      res.end(data);
-    });
-    
-  });
+var express = require("express");
+var app = express();
+var server = http.Server(app);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
