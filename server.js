7a9260942eb7510c9211eafb1fa9eaf50d33a838
Zinia Binte Jashim <1431069@iub.edu.bd>
2018-07-18 04:47:55 +0000

    classwork 7

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var http = require('http');
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 var server = http.Server(app);
+
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
 });
 app.get('/about',function(req,res){
   res.sendFile(__dirname+'/about.html');
+});
+app.get('/clss7',function(req,res){
+  res.sendFile(__dirname+'/clss7.html');
+});
+app.post('/submit_user',function(req,res)
+{
+    console.log(req.body);
 });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
