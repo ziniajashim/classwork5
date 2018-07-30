e6e9eb8c4685b86259d36d00f7cb7344ee8231e1
Zinia Binte Jashim <1431069@iub.edu.bd>
2018-07-30 04:28:02 +0000

    classwork 9

--- server.js
+++ server.js
@@ -5,25 +5,43 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 var server = http.Server(app);
-var mongo = require('mongodb');
-//for local
-//var db,url = "mongodb://localhost:27071";
-//for c9
-var db,url = "mongodb://"+process.env.IP+":28017/test";
+//var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://" + process.env.IP + ":27017/test");
 
-mongo.MongoClient.connect(url,{useNewUrlParser:true},function(err,client){
-  if(err){
-    console.log(err);
-    console.log("Could not connect to MongoDB");
-  }else{
-    db=client.db('node=cwD');
-  }
+mongoose.connection.on('erroe',function(){
+  console.log('Could not connect to mongodb');
 });
-var save = function(from_data){
-  db.createCollection('users',function(err,collection){});
-  var collection=db.collection('users');
-  collection.save(from_data);
-};
+//define schema with mongoose
+var userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: "Name is required"
+  } ,
+  email:String
+});
+var User = mongoose.model('User',userSchema);
+//for local
+//var db, url = "mongodb://localhost:27071";
+//for c9
+//var db, url = 'mongodb://' + process.env.IP + ":27017/test";
+//mongo.MongoClient.connect(url,
+ //           {useNewUrlParser:true},
+ //           function(err,client){
+ //             if(err){
+  //              console.log(err);
+  //              console.log("Could not connect to mongoDB");
+ //             }else{
+ //               db= client.db('node=cwD');
+//              }
+//            });
+//            
+//var save = function(from_data){
+ // db.createCollection('users',function(err, collection){});
+//  var collection= db.collection('users');
+//  collection.save(from_data);
+//};            
+
 
 
 app.get('/',function(req,res){
@@ -38,6 +56,16 @@ app.get('/clss7',function(req,res){
 app.post('/submit_user',function(req,res)
 {
     console.log(req.body);
+    var new_user = new User(req.body);
+    new_user.save(function(err,data){
+      if(err){
+        return res.status(400)
+                  .json({message:"couldn't save user"})
+      }
+      res.status(200).json(data);
+    })
+    //save(req.body);
+    res.status(200);
 });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
